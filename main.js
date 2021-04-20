9bd6a7b22ebcec2becc85d3e5cd60b73497a4e08
x2zh <zhang_xxiao@outlook.com>
2021-04-20 21:56:43 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hello deno deploy");
+addEventListener("fetch", (event) => {
+  const response = new Response("Hello World!", {
+    headers: { "content-type": "text/plain" },
+  });
+  event.respondWith(response);
+});
